a0e6a24f3a105ed2f8d4c9c45e9caf52c80e2352
jeanetteong <jeanetteongjx@hotmail.com>
2025-06-07 18:31:47 +0800

    07/06/25 welcome page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Photobooth Project
+functions to do:
+- different color scheme option
+- sound when clicking on button
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Picture from './Picture.jsx'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  const navigate = useNavigate()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className="welcome-title">
+        WELCOME TO <br />
+        <span className="photobooth-name">WTHELLY PHOTOBOOTH</span>
+      </h1>
+      <button className="start-button" onClick={() => navigate('/picture')}>
+        START
+      </button>
+    </div>
   )
 }
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/picture" element={<Picture />} />
+    </Routes>
+  )
+}
+
 
 export default App
